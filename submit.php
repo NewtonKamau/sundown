65b02332ca65ee9b7a262620067cb323b5483074
newton <kamaunewton78@gmail.com>
2021-02-03 11:13:26 +0300

    Changed the tagline and added the email submit page

---
+++ submit.php
@@ -0,0 +1,58 @@
+hello
+<!-- 
+    //$test = (isset($_POST['submit']) ? "yes" : "no";
+    // echo "yes";
+//echo "YOu are here"; 
+    // if(isset($_POST['submit']))
+    // {
+    //     $name = $_POST['name']; 
+    //     $email_id = $_POST['email']; 
+       
+    //     $msg = $_POST['message']; 
+         
+    //     $to = "kamaunewton78@gmail.com"; 
+    //     $subject = "'$name' has been sent a mail"; 
+         
+        // HTML Message Starts here
+        // $message ="
+        // <html>
+        //     <body>
+        //         <table style='width:600px;'>
+        //             <tbody>
+        //                 <tr>
+        //                     <td style='width:150px'><strong>Name: </strong></td>
+        //                     <td style='width:400px'>$name</td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td style='width:150px'><strong>Email ID: </strong></td>
+        //                     <td style='width:400px'>$email_id</td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td style='width:150px'><strong>Message: </strong></td>
+        //                     <td style='width:400px'>$msg</td>
+        //                 </tr>
+        //             </tbody>
+        //         </table>
+        //     </body>
+        // </html>
+        // ";
+        // HTML Message Ends here
+         
+        // Always set content-type when sending HTML email
+        // $headers = "MIME-Version: 1.0" . "\r\n";
+        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+          
+        // if(mail($to,$subject,$message,$headers)){
+            // Message if mail has been sent
+        //     echo "<script>
+        //             alert('Mail has been sent Successfully.');
+        //         </script>";
+        // }
+ 
+        // else{
+            // Message if mail has been not sent
+        //     echo "<script>
+        //             alert('EMAIL FAILED');
+        //         </script>";
+        // }
+    // } -->
